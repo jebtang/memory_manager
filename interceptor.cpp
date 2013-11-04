9a19b5db28370ca063d34d2bf7737934fc8ac90a
ravitandon <ravifreek63@gmail.com>
2013-11-03 19:38:54 -0500

    changes to interceptor code

--- interceptor.cpp
+++ interceptor.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include "stdio.h"
-#include "conio.h"
 #include "stdlib.h"
 #include <list>
 #include <string>
 #include <map>
 #include <ctype.h>
+#include <fstream>
 
 using namespace std;
 
-enum class_type  { IDENTIFIER, INTEGER, OPERATOR, STAR, WHITESPACE, MISCELLANEOUS }; // This is the class of elements seen
-int num_classes = 6;
+enum class_type  { IDENTIFIER, KEYWORD, INTEGER, OPERATOR, STAR, WHITESPACE, MISCELLANEOUS }; // This is the class of elements seen
+int num_classes = 7;
 
 // Token class maps each lexeme to its corresponding type
 class token {
-  public string lexeme;
-  public class_type type;
+public:
+  string lexeme;
+  class_type type;
+  void set (string s, class_type c){
+    this->lexeme = s;
+    this->type = c;
+  }
 };
 
 // This is a list of all the keywords which are present in the C programming langauge
@@ -53,8 +58,9 @@ char delimiters[] = {
 // The total number of such delimiters 
 int length_delimiters = sizeof (delimiters) / sizeof (char);
 
+// 'has_token_finished' checks the two characters c1, c2 and decides whether the token represented by c1 and its preceeding (if any) characters has ended because of char c2
 bool has_token_finished (char c1, char c2){
-  int count;
+  int count; // Temporary local variable
   // Checking for all the operators which have count > 2
   for (count = 0; count < num_operators; count++){
     if (operators[count].length() == 2 && c1 == operators[count][0] && c2 == operators[count][1])
@@ -65,13 +71,14 @@ bool has_token_finished (char c1, char c2){
     if (c2 == delimiters [count])
       return true;
    }
+  return false;
 }
 
 // This function checks whether the given str is a C keyword 
 bool is_keyword (string str){
   int count;
   for (count = 0; count < num_keywords; count++){
-    if (str.compare (keywords[count] == 0))
+    if (str.compare (keywords[count]) == 0)
 	return true;
   }
     return false;
@@ -80,65 +87,40 @@ bool is_keyword (string str){
 // This function returns the class type of a given token string 
 class_type get_class_type (string token){
   if (isdigit(token[0])) // We have assumed that our tokens for numbers have only integers in them
-    return class_type.INTEGER;
+    return INTEGER;
   if (is_keyword (token))
-    return class_type.KEYWORD;
+    return KEYWORD;
   if (isalpha(token[0])) // Tokens with alphabets which (other than keywords) are identifiers
-    return class_type.IDENTIFIER;
+    return IDENTIFIER;
   if (is_operator(token))
-    return class_type.OPERATOR;
+    return OPERATOR;
   if (token[0] == '*')
-    return class_type.STAR;
-  return class_type.MISCELLANEOUS;
+    return STAR; // STAR is identified as a special type of class 
+  return MISCELLANEOUS;
 }
 
-// This function extracts all the tokens from the given buffer and returns a list <tokens> 
-list <string> extract_tokens (string buffer){
-  list <string> token_list;
-  string current_token;
-  int count, token_size = 0, token_start, length = buffer.length();
+// This function extracts all the tokens from the given buffer and returns a list <token objects> 
+list <token> extract_tokens (string buffer){
+  list <token> token_map; // This is the list of token objects from each line
+  int count, token_start, length = buffer.length();
   for (count = 0; count < length; count++){
-    while (buffer[count] == ' '){
+    while (buffer[count] == ' '){ // Skipping whitespaces 
       count++;
     }
-    token_start = count; // index within the buffer where the token starts 
-    // Start of a new token  
-    current_token = '';
-    while ((count < length) && !has_token_finished(buffer[count], buffer[count+1])){
+    string current_token;  // Start of a new token  
+    while (((count < length - 1) && !has_token_finished(buffer[count], buffer[count+1])) || (count == length - 1)){
       current_token.push_back (buffer[count]);
+      count++;
     }
-    token_list.push_back (current_token);
+    token temp_token;
+    temp_token.set (current_token, get_class_type (current_token));
+    token_map.push_back (temp_token);
   }
+  return token_map;
 }
 
-// Searches for pointer (denoted by at least a single *) within the buffer. It returns -1 if no pointer has been found, else returns the value of the position where a star has been found 
-int find_pointer(char *buffer){
-  list <class_type> token_classes;
-  int count = 0, len = strlen (buffer);
-  bool pointer_found = false;
-  char ch, next_ch, pointer_location = -1; 
-  int classes[max_classes_per_line];
-  while (count < len){
-    
-    
-
-    if (ch == '*') { // seems like there is a pointer, there are two false positives 1) it could be a product literal 2)it could be part of a comment
-      pointer_location = count; // tentative location of the pointer 
-      // If it is a product literal surely there has to be a variable after it 
-      while (count < len){
-	next_ch = buf[++count];
-	if (is_alpha (next_ch)){ // Keep iterating over the next symbols 
-	  
-	}
-      }
-    }
-    count++;
-  }
-  return (pointer_found ? pointer_location : -1);
-} 
-
 // This function checks whether the given string str exists in the list (lstr) of strings 
-bool exists (list <string> lstr, string str){
+bool exists (string str, list <string> lstr){
   for (list<string>::iterator it = lstr.begin(); it != lstr.end(); it++){
     if (str.compare(*it) == 0)
       return true;
@@ -146,48 +128,81 @@ bool exists (list <string> lstr, string str){
   return false;
 }
 
-// Function which maps a list of classes to its tokens
-list <class_type> 
-
-// Function which parses the file and interceptors each memory access with an event to the runtime 
-void parse_file (fstream fs_in, fstream fs_out){
-  int count;
-  string buffer; // The buffer in which each of the line is read 
-  string token; // Temporary variable to store a token in 
-  list <string> dynamic_objects; // We need to maintain a list of all the dynamic variables by their names
-  list <token> tokens;   // Tokens from each line
-  list <class_type> token_classes; // This is a list of all the classes which correspond to each token 
-  while (getline(fs_in, buffer)){ // reading each line from the file , currently we assume each statement == each line
-    // removing all the empty lines, lines that are pragmas, header inclusions, single line comments 
-    if (strlen (buffer) == 0 || buffer[0] == '#' || (buffer[0] = '/' && buffer[1] = '/')) 
-      continue();
-    // In each line we check whether all the identifiers and see if any of them are dynamic variables
-    // Getting tokens from the current line and checking if they already exist as dynamic objects
-    tokens = extract_tokens (buffer);
-    // Check for two patterns currently <keyword, identifier (optional), *, identifier> then insert the token in the list of dynamic objects
-    token_classes = extract_token_classes (tokens);
-    for (count = 0;  count < tokens.size(); count++){
-      token = tokens[count];
-      if (exists(token, dynamic_objects)){
-	cout << token << " is dynamic variable" << endl;
-      }
-    }
+// Checks the order for an assignment to be pointer allocation (matches struct node * abc; type assignments) 
+bool check_order (class_type c, int position){
+  switch (position){
+  case 1:
+    return (c == KEYWORD);
+  case 2:
+    return (c == IDENTIFIER);
+  case 3:
+    return (c == STAR);
+  case 4:
+    return (c == IDENTIFIER);
+  default: 
+    return false;
   }
 }
 
+// Function which parses the file and interceptors each memory access with an event to the runtime 
+void parse_file(char *file_input, char *file_output){
+  ifstream fs_in (file_input);
+  if (!(fs_in.is_open())){
+    cout << "Input File" << file_input << "could not be opened." << endl;
+    return;
+  }
+  ofstream fs_out (file_output);
+  int count; // Temporary counter variable
+  string buffer; // The buffer in which each of the line is read 
+  string token_str; // Temporary variable to store a token in 
+  list <string> dynamic_objects; // We need to maintain a list of all the dynamic variables by their names
+  list <token> token_list;   // Tokens from each line
+  bool valid_dy_object = false; // Initially the no dynamic object has been seen, flag is set to false 
+  while (getline(fs_in, buffer)){ // reading each line from the file , currently we assume each statement == each line
+    // removing all the empty lines, lines that are pragmas, header inclusions, single line comments 
+    int len = buffer.size();
+    if (len == 0 || buffer[0] == '#' || (len > 1 && buffer[0] == '/' && buffer[1] == '/')) 
+      continue;
+    // In each line we check whether all the identifiers and see if any of them are dynamic variables
+    // Getting tokens from the current line and checking if they already exist as dynamic objects
+    token_list = extract_tokens (buffer);
+    // Check for two patterns currently <keyword, identifier (optional), *, identifier> then insert the token in the list of dynamic objects
+    count = 0,valid_dy_object = false ; string dynamic_object_lexeme;
+    for (list<token>::iterator it = token_list.begin(); it != token_list.end() && count < 4; it++){
+      count++;
+      if (count == 4)
+	dynamic_object_lexeme = it->lexeme;
+      valid_dy_object = check_order (it->type, count);
+      if (!(valid_dy_object) && count == 2 && it->type == STAR){
+	it++; 
+	valid_dy_object = (it->type == IDENTIFIER); // Checking the case when (int * x) kind of an assignment occurs.
+	dynamic_object_lexeme = it->lexeme;
+	break;
+      }
+      if(!(valid_dy_object)) // If the check fails anywhere in between a pointer allocation has not been found
+	break;
+    }
+    if (valid_dy_object){
+      dynamic_objects.push_back(dynamic_object_lexeme);
+    }
+    for (list <token>::iterator it = token_list.begin(); it != token_list.end(); it++){
+      token_str = it->lexeme;
+      if (exists(token_str, dynamic_objects)){
+	cout << token_str << " is dynamic variable" << endl;
+      }
+    }
+  }
+  fs_in.close ();
+  fs_out.close ();
+}
+
+// The main function
 int main(int argc, char** argv){
   if (argc < 3){  // the second argument (input file name must be passed as an argument)
     printf("Error!! Enter the file name to be parsed");
     return -1;
   }
-  // Opening the input file
-  fstream fs_in (argv[1], fstream::in);
-  // Annotated program is written to the output file  
-  fstream fs_out (argv[2], fstream::out);
-  // Parse the file to plugin all memory accesses with events 
-  parse_file (fs_in, fs_out);
-  // Closing the files
-  fs_in.close();
-  fs_out.close();
+  // Parse the file and annotate all memory accesses with events 
+  parse_file (argv[1], argv[2]);
   return 0;
 }
