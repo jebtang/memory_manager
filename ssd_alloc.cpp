5c02ec666598d3a05b1d315404de1d71d74fb217
ravitandon <ravifreek63@gmail.com>
2013-11-18 00:26:37 -0500

    ssd alloc changes

--- ssd_alloc.cpp
+++ ssd_alloc.cpp
@@ -1,34 +1,53 @@
-#include <time.h>       /* time */
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
+#include "errno.h"
 #include <sys/mman.h>
 #include <iostream>
 #include <map>
 #include <unistd.h>
 #include <signal.h>
 #include "ssd_alloc.h"
+#include "assert.h"
+
 using namespace std;
 
 #define handle_error(msg) \
   do {perror (msg); exit(EXIT_FAILURE);} while (0)
+
 #define PAGE_BUFFER_SIZE 10 // The page buffer has 10 pages in memory 
 int PAGE_SIZE =  sysconf(_SC_PAGE_SIZE); // Each page in page buffer is of size 4KB
 
-void *page_buffer; // This is the base location of the page buffer, location in memory where all the pages are materialized
+struct page_buffer_str {
+	void *page_buffer_ptrs[PAGE_BUFFER_SIZE]; // This is the array which stores the location of all the pages in the page buffer,
+	 // location in memory where all the pages are materialized
+	bool page_dirty[PAGE_BUFFER_SIZE]; // This keeps a track of whether each of the page in page buffer is dirty or not
+	bool page_buffer_bitmap[PAGE_BUFFER_SIZE]; // Have to convert it to an actual bitmap
+	int pagesInBuffer; // Keeps a count of the number of pages free in the page buffer
+};
 
-bool page_buffer_bitmap[PAGE_BUFFER_SIZE]; // Have to convert it to bitmap
+struct page_buffer_str page_buffer;
 struct sigaction sa;
 
 map <void*, struct object> object_table; // This is the object table which stores the exact location of objects to fetch them
 std::map<void *, struct object>::iterator object_table_it; // Iterator of the map
+
+// Function which converts a page header address to object's location
+void *page_header_to_object_va (void *ph){
+	return (void *)((struct page_header *)ph + 1);
+}
+
+// Function which converts an object's location to the location where its page header resides
+void *object_va_to_page_header(void *object_va){
+	return (void *)((struct page_header *)object_va - 1);
+}
 
 // This function updates the object table with a given object header
 void insert_object (struct page_header *ph, void *page_address){
   // Build the object from the page header 
   struct object ob = get_object_from_header (ph, page_address);
   // Insert the object into the object table 
-  object_table.insert (pair <void *, struct object> (ph->object_va, ob));
+  object_table.insert (pair <void *, struct object> (object_va_to_page_header (page_address), ob));
 }
 
 // This function gets an object from a page header
@@ -36,7 +55,7 @@ struct object get_object_from_header (struct page_header *ph, void *page_address
   struct object ob;
   void *start_object = (void *)((struct page_header *) page_address + 1);
   ob.size = ph->object_size;
-  ob.value = malloc (sizeof (ph->object_size));
+  ob.value = (void *) malloc (sizeof (ph->object_size));
   memcpy (ob.value, start_object, ph->object_size);
   return ob;
 }
@@ -56,28 +75,31 @@ void seg_handler(int sig, siginfo_t *si, void *unused){
 }
 
 void init_ssd_alloc (void){
-   // Initializing the page buffer to get memory space from for storing the pages in the page buffer 
-  int value = posix_memalign (&page_buffer, PAGE_SIZE, PAGE_BUFFER_SIZE * PAGE_SIZE);
-  if (value == EINVAL) 
-    handle_error("aligment has to be a power of 2");
-  if (value == ENOMEM) 
-    handle_error("out of memory");
-  // defining the seg fault handler 
+// Initializing the page buffer to get memory space from for storing the pages in the page buffer
+//  int value = posix_memalign (&page_buffer, PAGE_SIZE, PAGE_BUFFER_SIZE * PAGE_SIZE);
+//  if (value == EINVAL)
+//    handle_error("aligment has to be a power of 2");
+//  if (value == ENOMEM)
+//    handle_error("out of memory");
+
+  page_buffer.pagesInBuffer = PAGE_BUFFER_SIZE; // Initially the number of pages in the page buffer is equal to the total size of the page buffer
+// defining the segmentation fault handler
   sa.sa_flags = SA_SIGINFO; // The siginfo_t structure is passed as a second parameter to the user signal handler function
   sigemptyset(&sa.sa_mask); // Emptying the signal set associated with the structure sigaction_t 
   sa.sa_sigaction = seg_handler; // Assigning the fault handler 
   if (sigaction(SIGSEGV, &sa, NULL) == -1) // Installs the function in sa taken on a segmentation fault
     handle_error("sigaction");
-
 }
 
+// Constructs a new object of the given size
 struct object init_object(int size){
   struct object ob;
   ob.value = malloc (size); // the object is initialized
-  ob.size = size; // storing the size of the object
+  ob.size = size; 			// storing the size of the object
   return ob;
 }
 
+// Allocates an object and currently stores it in object table
 void *ssd_oalloc (int num_objects, int size_object){
   // Insert the object in object table
   // Allocating page in memory to the object, currently we implement it for objects of size < 4KB and num_objects = 1 
@@ -87,9 +109,13 @@ void *ssd_oalloc (int num_objects, int size_object){
   if (mprotect (object_location, PAGE_SIZE, PROT_NONE) == -1){
     handle_error("mprotect");
   }
-  return object_location;
+  // Initially the page is advised to be not needed
+  madvise (object_location, PAGE_SIZE, MADV_DONTNEED);
+  // Each of the page has a header which denotes the size of the object on the page
+  return (void *) ((struct page_header *) object_location + 1);
 }
 
+// The current eviction policy is based on random selection and eviction of a page from the page buffer.
 int select_random_page (void){
   /* initialize random seed: */
   srand (time(NULL));
@@ -98,25 +124,24 @@ int select_random_page (void){
 }
 
 // Function which gets a free page from the page buffer 
-void *get_free_page (void){
-  int count; void *page_address;
+int get_free_page (void){
+  int count;
+  void *page_address;
   for (count = 0; count < PAGE_BUFFER_SIZE; count++){
-    if (!(page_buffer_bitmap[count])){
-      break;
-    }
+    if (!(page_buffer.page_buffer_bitmap[count]))
+      return count;
   }
   // Checking if a free page has been found within the page buffer 
-  if (count < PAGE_BUFFER_SIZE){
-    return (void *)((char *) page_buffer + count * PAGE_SIZE);
-  } else {
+  	assert (count == PAGE_BUFFER_SIZE);
     printf ("page buffer is filled, evicting a page");
     // Page eviction is not done as yet, hence evicting the current page 
-    /* initialize random seed: */
-    
-    void *victim_page_address = (void *)((char *)page_buffer + select_random_page() * PAGE_SIZE); 
+    int victim_page_index = select_random_page();
+    void *victim_page_address = page_buffer.page_buffer_ptrs[victim_page_index] ;
+    printf ("victim page address = 0x%lx\n", (long) (victim_page_address));
     evict_page (victim_page_address);
-    return victim_page_address;
-  }
+    // Denoting that a page in page buffer is now empty
+    page_buffer.page_buffer_bitmap [victim_page_index] = true;
+    return victim_page_index;
 }
 
 /* This function evicts a victim page from the page buffer: */
@@ -128,13 +153,15 @@ void evict_page (void *victim_page_address){
   memcpy (ph, victim_page_address, sizeof(page_header));
   int object_size = ph->object_size;
   // Saving the object back to the object table
-  insert_object (ph, victim_page_address);  
+  insert_object (ph, victim_page_address);
+  // madvise command called to free this page
+  madvise (victim_page_address, PAGE_SIZE, MADV_DONTNEED);
 }
 
 // The page materialization function gets an argument as the address of the object and returns the address of the page 
 void materialize_page (void *va){
   // Getting a free page from the page buffer
-  void *free_page = get_free_page ();
+  int free_page_index = get_free_page ();
   // Getting the object our from the object table
   object_table_it = object_table.find (va);
   // Checking if no free page is found 
@@ -147,10 +174,9 @@ void materialize_page (void *va){
   // Constructing the page header 
   struct page_header *ph = (struct page_header *) malloc (sizeof(struct page_header)); // has to be freed later on 
   ph->object_size = ob.size; // assiging the object size to the page header
-  ph->object_va = va; // assigning the virtual address to the page header
-  memcpy(free_page, ph, sizeof (struct page_header)); // copying the page header to the page in the page buffer 
+  memcpy(va, ph, sizeof (struct page_header)); // copying the page header to the page in the page buffer
   free (ph); // the page header is freed here
-  memcpy((void *)((struct page_header*)free_page + 1), ob.value, ob.size); // copying the object to the materialized page 
+  memcpy((void *)((struct page_header*)va + 1), ob.value, ob.size); // copying the object to the materialized page
   // the memory allocated by to store the object in the object table is freed because on materializing an object the page has to be removed from the object table 
   free (ob.value); // this prevents memory leakage 
 }
--- ssd_alloc.h
+++ ssd_alloc.h
@@ -21,7 +21,6 @@ struct object {
 /* Each page has a header. The header currently stores the size of the object on the page. */
 struct page_header {
   int object_size;
-  void *object_va;
 };
 
 
@@ -30,9 +29,9 @@ void ssd_free (void *va_address);
 void seg_handler(int sig, siginfo_t *si, void *unused);
 void init_ssd_alloc (void);
 struct object init_object(int size);
-void *materialize_page (void *va);
+void materialize_page (void *va);
 void evict_page(void *);
-void *get_free_page(void);
+int get_free_page(void);
 int select_random_page(void);
 struct object init_object (int);
 struct object get_object_from_header (struct page_header *ph, void *page_address);
