18f90b67dbdf36046f8ff362bce31af7c3dc65ad
ravitandon <ravifreek63@gmail.com>
2013-11-18 03:45:13 -0500

    added Test folder

---
+++ Test/SSD_Alloc/test_create_object.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "../../ssd_alloc.h"
+#include "assert.h"
+using namespace std;
+
+void initialize_ssd_alloc (void){
+	init_ssd_alloc ();
+	printf ("Passed Initialization Test\n");
+}
+
+void create_object (void){
+	ssd_oalloc (1, 4);
+	printf ("Passed Object Creation Test\n");
+}
+
+struct node {
+	int value;
+};
+
+void update_object (){
+	struct node *abc = (struct node *) (ssd_oalloc (1, 4));
+	//abc->value = 2;
+	//assert (abc->value == 2);
+	printf ("Update Object Passed\n");
+}
+
+int main (){
+	initialize_ssd_alloc ();
+	create_object ();
+	update_object ();
+
+	return 0;
+}
