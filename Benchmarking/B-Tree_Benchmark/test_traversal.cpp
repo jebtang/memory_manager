ac4757d1ebfc660812a654080346e729722d5e98
ravitandon <ravifreek63@gmail.com>
2013-12-07 17:24:55 -0500

    added tests

---
+++ Benchmarking/B-Tree_Benchmark/test_traversal.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "b-tree.h"
+#include "time.h"
+#include <iostream>
+#include "../../ssd_alloc.h"
+#include <sys/time.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main (){  
+        cout << "printing the tree in order" << endl;
+        struct timeval start, end;
+        long mtime, seconds, useconds;  
+        init ();
+	int count, array_size = 1000*25;
+	int array[array_size];
+	srand(time(NULL));
+	for (count = 0; count < array_size; count++){
+	  array[count] = rand();
+	}
+	struct node *root = create_tree (array_size, 2, array);
+        gettimeofday(&start, NULL);
+	print_tree_pre_order (root);
+	gettimeofday(&end, NULL);
+	seconds  = end.tv_sec  - start.tv_sec;
+	useconds = end.tv_usec - start.tv_usec;
+	mtime = ((seconds) * 1000 + useconds/1000.0) + 0.5;
+	printf("Elapsed time: %ld milliseconds\n", mtime);
+	//	finish ();
+	return 0;
+}
